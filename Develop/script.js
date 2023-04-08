d8924e67f2d9d5f7a57753e266c90ff0c17361cc
Cole <122782745+cm-thom@users.noreply.github.com>
2023-04-07 22:38:31 -0400

    added password generator arrays and logic

--- Develop/script.js
+++ Develop/script.js
@@ -1,54 +1,61 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+var numbersArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var symbolsArr = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
 var lowercaseLettersStr = "abcdefghijklmnopqrstuvwxyz"
 
 var lowercaseLettersArr = lowercaseLettersStr.split('');
+//convert lowercaseLettersArr to uppercase
 var uppercaseLettersArr = lowercaseLettersStr.toUpperCase().split('');
 
-
-
-
-//write generate password function that prompts for:
-// 1. prompt user for password criteria
-//      a. password length, 8 - 128 characters
-//        - under 8 chars alerts password too short
-//        - over 128 chars alerts password too long
-//      b. prompt to in/exclude lowercase, uppercase, numbers, special chars 
-// 2. validate the input
-//    - ???????
-// 3. generate password based on user inputs
-//    - math.floor, math.random
-//    - ???????
-//    - wow so password
-// 4. display password to the page/screen. already done lmao
-
 function generatePassword() {
-  var passLength = prompt("Please specify password length between 8-128 characters:", "8-128 characters");
+  selectionArr = [];
+  var passLength = prompt("Please specify password length between 8-128 characters:", "");
 
-  if(passLength < 8 || passLength > 128) {
-    alert("try again dummy");
+//validates that user input is a number between 8 and 128, throws alert if validation fails (passes?) 
+  if(passLength < 8 || passLength > 128 || isNaN(passLength)) {
+    alert("Password is too long or short, or not a number. Please try again.");
+    return false;
   }
 
-
-  return "generated password variable goes here"
+//prompts to include/exclude data types, throws alert if none selected
+if(confirm("Include lowercase letters?")) {
+  selectionArr = selectionArr.concat(lowercaseLettersArr);
 }
+if(confirm("Include uppercase letters?")) {
+  selectionArr = selectionArr.concat(uppercaseLettersArr);
+}
+if(confirm("Include special characters?")) {
+  selectionArr = selectionArr.concat(symbolsArr);
+}
+if(confirm("Include numbers?")) {
+  selectionArr = selectionArr.concat(numbersArr);
+}
+if(selectionArr = []) {
+  alert("You must select at least one data type!");
+  return false;
+}
+
+//I understand this is not an optimal solution to remove the odd JS-targeting-the-password-id-element text, but I don't know the correct way to solve that problem yet.
+password = " ";
+
+
+//builds password variable to be printed via passwordText in writePassword function
+for( i = 0; i < passLength; i++) {
+  var passChar = Math.floor(Math.random() * selectionArr.length);
+  password = password + selectionArr[passChar];
+}
+return password
+}
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-//make generate password function to return a value for var password in writePassword function
-
-//figure out how tf document.querySelector("password") works
